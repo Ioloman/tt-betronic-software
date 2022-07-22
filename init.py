23a501a99d76bbbd0fb9257426b94fe5bcc1ee44
kirill <kirill.dudchenko@zenzero.ae>
2022-07-22 18:55:50 +0700

    fix logger file creation;

--- init.py
+++ init.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 
@@ -13,12 +14,16 @@ def init(file: str) -> str:
     load_dotenv('.env')
 
     # configure logger
-    logger_name = file.removesuffix('py')
+    logger_name = os.path.split(file)[1].removesuffix('.py')
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
     logger.propagate = False
+    # create directory
+    log_path = os.path.join(os.getenv('LOGGER_DIR'), logger_name + '.log')
+    if not os.path.exists(os.path.dirname(log_path)):
+        Path(os.path.dirname(log_path)).mkdir(parents=True, exist_ok=True)
     # create a file handler
-    handler = logging.FileHandler(os.path.join(os.getenv('LOGGER_DIR'), logger_name + 'log'))
+    handler = logging.FileHandler(log_path)
     handler.setLevel(logging.INFO)
 
     # create a logging format
