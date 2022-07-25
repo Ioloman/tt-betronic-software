3b2382e3a2c8477b09767773a71d932c195534c7
kirill <kirill.dudchenko@zenzero.ae>
2022-07-25 14:02:19 +0700

    improve cache function;

--- helpers.py
+++ helpers.py
@@ -26,7 +26,7 @@ async def update_cached_data(redis: Redis, key: str, data: str | bytes):
     """
     Update data of key and keep remaining expiration time
     """
-    await redis.set(key, data, keepttl=True)
+    await redis.set(key, data, xx=True, keepttl=True)
 
 
 async def get_events_cached(redis: Redis, tasks: BackgroundTasks) -> list[dict]:
@@ -37,19 +37,27 @@ async def get_events_cached(redis: Redis, tasks: BackgroundTasks) -> list[dict]:
     events = await redis.get(os.getenv('EVENTS_REDIS_KEY'))
     if events is not None:
         # filter and update if changed
-        now = datetime.datetime.now()
-        events_filtered = list(filter(lambda e: e['deadline'] > now, json.loads(events)))
-        if len(events_filtered) < len(events):
-            tasks.add_task(update_cached_data, redis, os.getenv('EVENTS_REDIS_KEY'), json.dumps(events_filtered))
+        now = datetime.datetime.now().isoformat()
+        events_parsed = json.loads(events)
+        events_filtered = list(filter(lambda e: e['deadline'] > now, events_parsed))
+        if len(events_filtered) < len(events_parsed):
+            tasks.add_task(
+                update_cached_data, redis,
+                os.getenv('EVENTS_REDIS_KEY'), json.dumps(events_filtered)
+            )
         return events_filtered
 
     # if got nothing from cache - get from line-provider service
     async with httpx.AsyncClient() as client:  # type: httpx.AsyncClient
         response = await client.get(urllib.parse.urljoin(os.getenv('EVENTS_API_URL'), '/events?current=true'))
-
+    if response.status_code != 200:
+        return []
     # if got non empty data - update cache
     if data := response.json():
-        tasks.add_task(cache_data, redis, os.getenv('EVENTS_REDIS_KEY'), response.text, int(os.getenv('EVENTS_REDIS_EX')))
+        tasks.add_task(
+            cache_data, redis,
+            os.getenv('EVENTS_REDIS_KEY'), response.text, int(os.getenv('EVENTS_REDIS_EX'))
+        )
 
     return data
 
