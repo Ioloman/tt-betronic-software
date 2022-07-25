60350455a06f80d5cae144abd959d99cdd1a9e84
kirill <kirill.dudchenko@zenzero.ae>
2022-07-25 12:46:54 +0700

    update caching function;

--- helpers.py
+++ helpers.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import os
 import urllib.parse
@@ -15,17 +16,38 @@ from models import EventStatusUpdate, Bet
 
 
 async def cache_data(redis: Redis, key: str, data: str | bytes, ex: int):
+    """
+    Set data to key in redis with given expiration time
+    """
     await redis.set(key, data, ex=ex)
 
 
-async def get_events_cached(redis: Redis, tasks: BackgroundTasks) -> dict:
+async def update_cached_data(redis: Redis, key: str, data: str | bytes):
+    """
+    Update data of key and keep remaining expiration time
+    """
+    await redis.set(key, data, keepttl=True)
+
+
+async def get_events_cached(redis: Redis, tasks: BackgroundTasks) -> list[dict]:
+    """
+    Get events from cache if possible
+    """
+    # get from cache
     events = await redis.get(os.getenv('EVENTS_REDIS_KEY'))
     if events is not None:
-        return json.loads(events)
+        # filter and update if changed
+        now = datetime.datetime.now()
+        events_filtered = list(filter(lambda e: e['deadline'] > now, json.loads(events)))
+        if len(events_filtered) < len(events):
+            tasks.add_task(update_cached_data, redis, os.getenv('EVENTS_REDIS_KEY'), json.dumps(events_filtered))
+        return events_filtered
 
+    # if got nothing from cache - get from line-provider service
     async with httpx.AsyncClient() as client:  # type: httpx.AsyncClient
         response = await client.get(urllib.parse.urljoin(os.getenv('EVENTS_API_URL'), '/events?current=true'))
 
+    # if got non empty data - update cache
     if data := response.json():
         tasks.add_task(cache_data, redis, os.getenv('EVENTS_REDIS_KEY'), response.text, int(os.getenv('EVENTS_REDIS_EX')))
 
